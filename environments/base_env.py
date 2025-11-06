fde6db9060179ce0a7d83fa310f6c6abe857768a
Hitesh <hiteshchavan1305@gmail.com>
2025-11-06 22:29:41 +0530

    file size

--- environments/base_env.py
+++ environments/base_env.py
@@ -1,34 +1,3 @@
-# environments/base_env.py
-from abc import ABC, abstractmethod
-
-class BaseEnvironment(ABC):
-    """
-    Standardized environment interface:
-    - reset() -> initial_state
-    - step(action) -> (next_state, reward, done, info)
-    - get_valid_actions() -> list
-    - render() for debug or headless printouts
-    """
-    def __init__(self):
-        self.state = None
-        self.done = False
-        self.winner = None
-
-    @abstractmethod
-    def reset(self):
-        pass
-
-    @abstractmethod
-    def step(self, action):
-        pass
-
-    @abstractmethod
-    def get_valid_actions(self):
-        pass
-
-    @abstractmethod
-    def render(self):
-        pass
-
-    def is_terminal(self):
-        return self.done
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0fe35d5658a1edb05f36fc4a47f68938f7af5bdc7fb0e1d6bd2c2eb25baaba6
+size 745
--- utils/reply_buffer.py
+++ utils/reply_buffer.py
@@ -1,25 +1,3 @@
-# utils/replay_buffer.py
-import random
-from collections import deque
-import numpy as np
-
-class ReplayBuffer:
-    def __init__(self, capacity=100_000):
-        self.buffer = deque(maxlen=capacity)
-
-    def push(self, state, action, reward, next_state, done):
-        self.buffer.append((
-            np.array(state, dtype=np.float32),
-            int(action),
-            float(reward),
-            np.array(next_state, dtype=np.float32),
-            float(done)
-        ))
-
-    def sample(self, batch_size):
-        batch = random.sample(self.buffer, batch_size)
-        s, a, r, ns, d = zip(*batch)
-        return (np.stack(s), np.array(a), np.array(r), np.stack(ns), np.array(d))
-
-    def __len__(self):
-        return len(self.buffer)
+version https://git-lfs.github.com/spec/v1
+oid sha256:21c424bfe47a11293375b6c974cacf3a7df3504086fc03de2dbb21fc879d0c4b
+size 763
